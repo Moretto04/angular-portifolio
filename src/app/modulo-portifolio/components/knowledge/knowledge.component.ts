68a584bbd09846c67adbd157d5570da4b5b4bf5c
Tiago Alves <tiago.alvessilva67@gmail.com>
2024-02-20 07:52:34 -0300

    atualizando o componente knowledge

--- src/app/modulo-portifolio/components/knowledge/knowledge.component.ts
+++ src/app/modulo-portifolio/components/knowledge/knowledge.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-knowledge',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './knowledge.component.css'
 })
 export class KnowledgeComponent {
-
+  public arrayKnowledge = signal([
+    {
+      src: 'assets/knowledge/html.svg',
+      alt: 'icone html',
+    },
+    {
+      src: 'assets/knowledge/css.svg',
+      alt: 'icone css',
+    },
+    {
+      src: 'assets/knowledge/javascript.svg',
+      alt: 'icone javascript',
+    },
+    {
+      src: 'assets/knowledge/angular.svg',
+      alt: 'icone angular',
+    },
+  ])
 }
