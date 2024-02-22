00fc71913632d81df8d446f9e4d1887d97bf5794
Tiago Alves <tiago.alvessilva67@gmail.com>
2024-02-21 22:20:28 -0300

    componente projects

---
+++ src/app/modulo-portifolio/components/experiences/experiences.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.css'
+})
+export class ExperiencesComponent {
+  public arrayExperiences = signal ([
+    {
+      summary: {
+        strong: 'Estagiário de desenvolvimentoFront-End',
+        p: 'BBTS | Abr 2023 - Set 2023',
+      },
+      text: 'Atuar no desenvolvimento web com a criação de páginas web e principalmente a criação de componentes documentados no StoryBook',
+    },
+  ]);
+}
