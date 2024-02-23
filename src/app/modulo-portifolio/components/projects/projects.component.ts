2d0629a70444cc091202c1a23c5a3009e6736c87
Tiago Alves <tiago.alvessilva67@gmail.com>
2024-02-23 07:37:41 -0300

    terminado os componentes e adicionando estilo

--- src/app/modulo-portifolio/components/projects/projects.component.ts
+++ src/app/modulo-portifolio/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  public arrayProjects = signal([
+    {
+      src: 'assets/header/tiago-pqueno.jpeg',
+      alt: 'projeto',
+      title: 'projeto',
+      width: '100px',
+      height: '51px',
+      description: 'uma descrição qualquer',
+      links: [
+        {
+          name: 'conheça o blog',
+          href: ''
+        }
+      ]
+    }
+  ])
 }
