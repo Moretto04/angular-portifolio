86724612dc957beaffed4563a2afd26d38cac946
Tiago Alves <tiago.alvessilva67@gmail.com>
2024-03-15 23:03:05 -0300

    atualizando o projects

--- src/app/modulo-portifolio/components/projects/projects.component.ts
+++ src/app/modulo-portifolio/components/projects/projects.component.ts
@@ -1,39 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
-
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
-import { IProjects } from '../../interface/IProjecrs.interface';
-import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-  #dialog = inject(MatDialog);
-  public arrayProjects = signal<IProjects[]>([
-    {
-      src: 'assets/header/tiago-pqueno.jpeg',
-      alt: 'projeto',
-      title: 'Projeto 1',
-      width: '100px',
-      height: '51px',
-      description: 'Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.',
-      links: [
-        {
-          name: 'conheça o blog',
-          href: ''
-        }
-      ]
-    },
-  ])
-  public openDialog(data: IProjects){
-    this.#dialog.open(DialogProjectsComponent,{
-      data,
-      panelClass: EDialogPanelClass.PROJECTS,
-    });
-  }
 }
